56cb215469fed0b88280a08999c9b11965d2ad08
Bruno Basto <bruno.basto@liferay.com>
2018-08-17 17:43:09 -0300

    LPS-84298 General SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/components/Page/__tests__/PageRenderer.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/components/Page/__tests__/PageRenderer.es.js
@@ -4,10 +4,10 @@ import './__fixtures__/Fields.es';
 import Context from './__mock__/mockContext.es';
 import PageRenderer from '../PageRenderer.es';
 
-let component;
-let context = null;
-let componentProps = null;
 const spritemap = 'icons.svg';
+let component;
+let componentProps = null;
+let context = null;
 
 describe(
 	'PageRenderer',
@@ -20,8 +20,8 @@ describe(
 					contentRenderer: 'grid',
 					editable: true,
 					page: context,
-					spritemap,
 					pageId: 0,
+					spritemap,
 					total: 1
 				};
 
@@ -42,7 +42,7 @@ describe(
 				expect(component).toMatchSnapshot();
 			}
 		);
-		
+
 		it(
 			'should display empty drag message when there are rows with no columns specified',
 			() => {
@@ -67,8 +67,8 @@ describe(
 					}
 				);
 
-				const spy = jest.spyOn(component, 'emit');
 				const pageTitle = component.element.querySelector('.form-builder-page-header-title');
+				const spy = jest.spyOn(component, 'emit');
 
 				pageTitle.value = 'Page Title';
 
@@ -91,10 +91,10 @@ describe(
 					}
 				);
 
-				const spy = jest.spyOn(component, 'emit');
 				const pageDescription = component.element.querySelector('.form-builder-page-header-description');
+				const spy = jest.spyOn(component, 'emit');
 
-				pageDescription.value = "Page Description";
+				pageDescription.value = 'Page Description';
 
 				jest.runAllTimers();
 				MetalTestUtil.triggerEvent(pageDescription, 'keyup', {});
@@ -148,7 +148,7 @@ describe(
 				component = new PageRenderer(
 					{
 						...componentProps,
-						contentRenderer: 'list',
+						contentRenderer: 'list'
 					}
 				);
 
@@ -166,7 +166,7 @@ describe(
 			() => {
 				component = new PageRenderer(
 					{
-						...componentProps,
+						...componentProps
 					}
 				);
 
@@ -185,7 +185,7 @@ describe(
 				component = new PageRenderer(
 					{
 						...componentProps,
-						dragAndDropDisabled: true,
+						dragAndDropDisabled: true
 					}
 				);
 
