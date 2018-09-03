a6dcf2ca54cd37e4158fb9770e9478d640edbae7
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2018-09-03 19:37:40 -0300

    LPS-84991 Create tests

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/pages/builder/__tests__/Builder.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/resources/META-INF/resources/metal/js/pages/builder/__tests__/Builder.es.js
@@ -87,20 +87,6 @@ describe(
 		);
 
 		it(
-			'should continue to propagate the deleteField event',
-			() => {
-				const spy = jest.spyOn(component, 'emit');
-				const {FormRenderer} = component.refs;
-				const mockEvent = jest.fn();
-
-				FormRenderer.emit('deleteButtonClicked', mockEvent);
-
-				expect(spy).toHaveBeenCalled();
-				expect(spy).toHaveBeenCalledWith('deleteField', expect.anything());
-			}
-		);
-
-		it(
 			'should continue to propagate pagesUpdated event',
 			() => {
 				const spy = jest.spyOn(component, 'emit');
@@ -188,6 +174,42 @@ describe(
 				jest.runAllTimers();
 
 				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should show modal when trash button gets clicked',
+			() => {
+				const {FormRenderer} = component.refs;
+				const mockEvent = jest.fn();
+
+				FormRenderer.emit('deleteFieldClicked', mockEvent);
+
+				const modal = document.querySelector('.modal');
+
+				jest.runAllTimers();
+
+				expect(modal.classList.contains('show')).toEqual(true);
+
+				expect(component).toMatchSnapshot();
+			}
+		);
+
+		it(
+			'should emit deleteField event when yes is clicked in the modal',
+			() => {
+				const spy = jest.spyOn(component, 'emit');
+				const {FormRenderer} = component.refs;
+				const mockEvent = jest.fn();
+
+				FormRenderer.emit('deleteFieldClicked', mockEvent);
+
+				component.element.querySelectorAll('.modal-content .btn-group .btn-group-item button')[1].click();
+
+				jest.runAllTimers();
+
+				expect(spy).toHaveBeenCalled();
+				expect(spy).toHaveBeenCalledWith('deleteField', expect.anything());
 			}
 		);
 	}
